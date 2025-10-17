a127f782d5684c4163faa81b861526e3ae6696aa
Selith Rubasingha <selithrubasingha@gmail.com>
2025-10-17 21:46:20 +0530

    added comments for the backend

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User # we don't need to make a User , we already have User built in bro
 # Create your models here.
 
 
 class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)#auto_now_add gives the date and time of creation of this data
     author = models.ForeignKey(User,on_delete=models.CASCADE , related_name="notes")
+    #related name is useful when i know the user and I need to get the user's notes
+    #I can just type user.notes.all() .
 
     def __str__(self):
         return self.title
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,25 +1,33 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Note
-
+# serializers are used to send data from frontend to backend 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "password"]
-        extra_kwargs = {"password": {"write_only": True}}
+        fields = ["id", "username", "password"] #the fields of data about to send to frontend
+        extra_kwargs = {"password": {"write_only": True}} #extra info : the password cannot be changed in the frontend 
+        #it's write only bro !
 
+    #when data is sent from the frontend to backend , that data is validated data! and and using that we
+    #create a User in the backend
     def create(self, validated_data):
         print(validated_data)
+        #BOOM ! user created baby! this syntax is in the build in user I guess
         user = User.objects.create_user(**validated_data)
         return user
     
 
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
+        #just normal meta data
         model = Note
         fields = ["id", "title", "content", "created_at", "author"]
         extra_kwargs = {"author": {"read_only": True}}
 
+    #why isn't there a create function for notes?
+    # well this uses the build in create user function that is provided by 
+    # MidelSerializer 
 
 
 
