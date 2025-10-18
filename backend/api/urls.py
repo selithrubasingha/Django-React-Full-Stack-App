b93612809680ed557fab7b05ffe55184b3fa174c
Selith Rubasingha <selithrubasingha@gmail.com>
2025-10-18 11:46:07 +0530

    added some useful comments for understanding code

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path("notes/",views.NoteListCreate.as_view() , name="note-list"),
     path("notes/delete/<int:pk>",views.NoteDelete.as_view() , name="delete-note")
+    #pk stands for primary key . why isn't it given as a argument for delete note view??
+    #becuase the whole thing is automated . the function automatically uses pk to search for 
+    # the note to delete . that is the difference between FunctionBasedViews and 
+    #ClassBasedViews
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -6,6 +6,13 @@ from rest_framework.permissions import IsAuthenticated,AllowAny
 from .models import Note
 
 # Create your views here.
+
+#generics in rest_framework pretty much does most of the stuff for you
+#ListCreate api view allows you to list all the model data and create all the. model data
+#but since this is a multi user platform , we need to modify the built in get_queryset and perform create views.
+#for get query : you gotta filter them by author , otherwise the user can see EVERYBODY'S messages
+#as for perform create you gotta give it the author data because the json data input is not going to give you
+#that info.
 class NoteListCreate(generics.ListCreateAPIView):
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
@@ -20,7 +27,13 @@ class NoteListCreate(generics.ListCreateAPIView):
         else:
             print(serializer.errors)
 
+#why did we modify the get query here? becuase we eed to make sure the relavent user's note is deleted.
+# what if the user asked to delete id.5 note . but if query set was all the notes of all the users . 
+# os A user could mistakenly delete B user's id.5 note .
 
+#also why can't we use queryset = Note.objects.filter(author=user) on the top ? because this is a class .
+# and the user is defined in the http request . the program doesn't know who the user is a the start of the 
+#program
 class NoteDelete(generics.DestroyAPIView):
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
@@ -29,7 +42,9 @@ class NoteDelete(generics.DestroyAPIView):
         user = self.request.user
         return Note.objects.filter(author=user)
 
+#no modification needed this is a create API view
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [AllowAny] #AllowAny is used bacause anyone should be able to create an account not 
+    #just the authorized people
